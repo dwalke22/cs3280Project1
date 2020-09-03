be82fd1822de02cd260edf2e5a0dbb05af80779b
dwalke22 <dwalke22@cs3280-vm07.uwg.westga.edu>
2020-09-03 18:19:59 -0400

    Started the main python script

---
+++ main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+
+"""
+
+
+__author__ = "David Walker"
+__verison__ = "Fall 2020"
+
+def main():
+    """Main entry point of program."""
+
+if __name__ == "__main__":
+    main()
