6c7d001f6db7603e861de40056cca33a3982e7b2
dwalke22 <dwalke22@cs3280-vm07.uwg.westga.edu>
2020-09-03 18:37:55 -0400

    Completed main script Docs and ran small execution

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python3
 """
+Script to verify a valid credit card.
 
+Credit Card Verification
+Loads standard credit card types from a .ssv file
+(semi-colon seperated values) to be used to identify
+the type of credit card. The type of card will be used
+and Luhn's algorithm will be used to verify that the credit
+card is genuine.
 """
 
 
@@ -9,6 +16,7 @@ __verison__ = "Fall 2020"
 
 def main():
     """Main entry point of program."""
+    print("No Credit Card is Valid")
 
 if __name__ == "__main__":
     main()
