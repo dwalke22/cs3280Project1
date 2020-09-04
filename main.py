580a09e450c2ae62380490fe43715f70aea2b84b
dwalke22 <dwalke22@cs3280-vm05.uwg.westga.edu>
2020-09-04 15:06:06 -0400

    Added a function in main.py that checks for valid input

--- main.py
+++ main.py
@@ -15,13 +15,22 @@ import re
 __author__ = "David Walker"
 __verison__ = "Fall 2020"
 
+def verify_credit_number(credit_card_number):
+    """Verifies that the credit card number follows is a normal number"""
+    credit_card_regex = re.compile(r'\d{13,19}|(\d{4}-){3}\d{4}|(\d{4} ){3}\d{4}')
+    match = credit_card_regex.match(credit_card_number)
+    valid = ''
+    if match:
+        valid = match.group()
+    else:
+        valid = 'Invalid'
+    return valid
+
 def main():
     """Main entry point of program."""
-    credit_card_regex = re.compile(r'\d{13,19}|(\d{4}-){3}\d{4}|(\d{4} ){3}\d{4}')
     print("Please enter a credit card number:")
     credit_card_number = input()
-    match = credit_card_regex.search(credit_card_number)
-    print(match)
+    print('Credit Card Number:\t' + verify_credit_number(credit_card_number))
 
 if __name__ == "__main__":
     main()
