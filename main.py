36a5048d885805fa8433cb8021505871ac021e3e
dwalke22 <dwalke22@cs3280-vm01.uwg.westga.edu>
2020-09-03 21:49:09 -0400

    Completed CC# Regex and tested by running

--- main.py
+++ main.py
@@ -10,13 +10,18 @@ and Luhn's algorithm will be used to verify that the credit
 card is genuine.
 """
 
+import re
 
 __author__ = "David Walker"
 __verison__ = "Fall 2020"
 
 def main():
     """Main entry point of program."""
-    print("No Credit Card is Valid")
+    credit_card_regex = re.compile(r'\d{13,19}|(\d{4}-){3}\d{4}|(\d{4} ){3}\d{4}')
+    print("Please enter a credit card number:")
+    credit_card_number = input()
+    match = credit_card_regex.search(credit_card_number)
+    print(match)
 
 if __name__ == "__main__":
     main()
---
+++ utils.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+"""
+
+
+def luhn_verified(credit_card_number):
+
+
+def is_valid(sequence):
+
