dc8616489c48ca5a96535fbe7e818a2cc0b25460
dwalke22 <dwalke22@cs3280-vm07.uwg.westga.edu>
2020-09-05 22:33:38 -0400

    Added ssv read and seperation of lines into proper storage

--- main.py
+++ main.py
@@ -26,11 +26,44 @@ def verify_credit_number(credit_card_number):
         valid = 'Invalid'
     return valid
 
+def seperate_lengths(values):
+    """Seperates different number length values if any"""
+    if "," in values:
+        values = values.split(",")
+    return values
+
+def seperate_prefix(values):
+    """Seperates different credit card numbers if any"""
+    if "," in values:
+        values = values.split(",")
+    return values
+
+def seperate_issuers(lines):
+    """Seperates each issuer into the different properties"""
+    issuers = []
+    for line in lines:
+        values = line.split(";")
+        issuer = {'network': values[0], 'length': seperate_lengths(values[1]),\
+                'prefix': seperate_prefix(values[2])}
+        issuers.append(issuer)
+    return issuers
+
+
+def load_card_types():
+    """Loads a ssv file and returns the seperated lines in a list of dictionaries"""
+    types = open('credit_card_types.ssv')
+    lines = types.readlines()
+    issuers = seperate_issuers(lines)
+    types.close()
+    return issuers
+
 def main():
     """Main entry point of program."""
     print("Please enter a credit card number:")
     credit_card_number = input()
     print('Credit Card Number:\t' + verify_credit_number(credit_card_number))
+    issuers = load_card_types()
+    print(issuers)
 
 if __name__ == "__main__":
     main()
